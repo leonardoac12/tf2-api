af5d8a8436d3d374be3874166314d612fa6ccc86
leonardoac12 <leonardoarcr@outlook.es>
2025-08-18 09:32:30 -0500

    chore(config): estructura básica.

---
+++ tf2-api/src/main/java/com/github/leonardoac12/tf2_api/Tf2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.leonardoac12.tf2_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tf2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tf2ApiApplication.class, args);
+	}
+
+}
