be2d440ab1d3426aca7c5d1e9fced0c5bcccfd2c
leonardoac12 <leonardoarcr@outlook.es>
2025-10-09 17:49:51 -0500

    CosmeticController.java

--- tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/CosmeticController.java
+++ tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/CosmeticController.java
@@ -1,7 +1,12 @@
 package com.github.leonardoac12.tf2_api.controller;
 
+import com.github.leonardoac12.tf2_api.model.Cosmetic;
+import com.github.leonardoac12.tf2_api.model.Weapon;
 import com.github.leonardoac12.tf2_api.repository.ICosmeticRepository;
+import com.github.leonardoac12.tf2_api.service.ICosmeticService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CosmeticController {
 
     @Autowired
-    private ICosmeticRepository cosmeticRepository;
+    private ICosmeticService cosmeticService;
+
+    @PostMapping("/create")
+    public String createCosmetic(@RequestBody Cosmetic cosmetic) {
+        cosmeticService.saveCosmetic(cosmetic);
+        return "";
+    }
 }
