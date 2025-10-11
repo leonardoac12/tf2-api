0b8ffc7a42e95bf6c9f6345fa8fcaf746cc92322
leonardoac12 <leonardoarcr@outlook.es>
2025-10-11 18:32:49 -0500

    act controllers mapping

--- tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/CosmeticController.java
+++ tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/CosmeticController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cosmetics")
+@RequestMapping("/tf2-api/cosmetics")
 public class CosmeticController {
 
     @Autowired
--- tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/WeaponController.java
+++ tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/WeaponController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/weapons")
+@RequestMapping("/tf2-api/weapons")
 public class WeaponController {
 
     @Autowired
