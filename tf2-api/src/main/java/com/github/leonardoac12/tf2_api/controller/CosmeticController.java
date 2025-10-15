5857d725942550abc65e6a30c868fc58b0c2c977
leonardoac12 <leonardoarcr@outlook.es>
2025-10-14 22:44:08 -0500

    act CosmeticController.java

--- tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/CosmeticController.java
+++ tf2-api/src/main/java/com/github/leonardoac12/tf2_api/controller/CosmeticController.java
@@ -1,8 +1,6 @@
 package com.github.leonardoac12.tf2_api.controller;
 
 import com.github.leonardoac12.tf2_api.model.Cosmetic;
-import com.github.leonardoac12.tf2_api.model.Weapon;
-import com.github.leonardoac12.tf2_api.repository.ICosmeticRepository;
 import com.github.leonardoac12.tf2_api.service.ICosmeticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
